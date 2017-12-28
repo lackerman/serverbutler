da3ba7afd45d9d16591acf2ec83273471ed3ed7d
Leo Ackerman <ackerman.leo@gmail.com>
2017-12-28 21:15:47 +0100

    Changed the default port and made the config_dir configurable

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/lackerman/serverbutler/controllers"
 	"github.com/lackerman/serverbutler/utils"
@@ -18,12 +19,12 @@ func main() {
 
 	// The returned DB instance is safe for concurrent use. Which means that all
 	// DB's methods may be called concurrently from multiple goroutines.
-	db, err := leveldb.OpenFile("/tmp/db", nil)
+	db, err := leveldb.OpenFile(os.Getenv("config_dir"), nil)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	controllers.RegisterRoutes(templates, db)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
