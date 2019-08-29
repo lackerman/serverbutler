d2a498575ea9661385e99269efd826be8cb973a4
Leo Ackerman <ackerman.leo@gmail.com>
2019-08-29 19:47:12 +0200

    switched to modules and stop embedding assets in the binary

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lackerman/serverbutler
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/syndtr/goleveldb v1.0.0
+)
--- main.go
+++ main.go
@@ -1,24 +1,20 @@
-//go:generate go-bindata -ignore ".*.swp|.DS_Store" -pkg utils -o utils/assets.go templates
 package main
 
 import (
 	"flag"
 	"fmt"
+	"github.com/lackerman/serverbutler/handlers"
 	"github.com/syndtr/goleveldb/leveldb"
-	"gitlab.com/lackerman/serverbutler/handlers"
-	"gitlab.com/lackerman/serverbutler/utils"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "The port to use for the server (default: 8080)")
-	path := flag.String("path", "./tmp", "The file path to use for level db")
+	path := flag.String("path", "./bin/tmp", "The file path to use for level db")
 	flag.Parse()
 
-	templates, err := utils.ParseTemplatesFromBinData("templates")
-	if err != nil {
-		panic(err.Error())
-	}
+	templates := template.Must(template.ParseGlob("templates/*"))
 	// The returned DB instance is safe for concurrent use. Which means that all
 	// DB's methods may be called concurrently from multiple goroutines.
 	db, err := leveldb.OpenFile(*path, nil)
